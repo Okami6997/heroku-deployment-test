7bc74656f71257589a72bc4d89403a7b5c4481c9
Ashutosh Krishna Jha <ashutoshkrishnajha@Ashutoshs-Air.Dlink>
2022-10-15 19:43:42 +0530

    Initial Commit

---
+++ src/main/java/com/test/herokudeploytest/HerokuDeployTestApplication.java
@@ -0,0 +1,13 @@
+package com.test.herokudeploytest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HerokuDeployTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HerokuDeployTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/herokudeploytest/HerokuDeployTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.herokudeploytest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HerokuDeployTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

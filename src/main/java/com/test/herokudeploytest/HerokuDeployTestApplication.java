c102bbe0b760c4583b0e1448763358f54b00459a
Ashutosh Krishna Jha <ashutoshkrishnajha@Ashutoshs-Air.Dlink>
2022-10-15 19:53:16 +0530

    add autoconf

--- src/main/java/com/test/herokudeploytest/HerokuDeployTestApplication.java
+++ src/main/java/com/test/herokudeploytest/HerokuDeployTestApplication.java
@@ -1,9 +1,11 @@
 package com.test.herokudeploytest;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class HerokuDeployTestApplication {
 
 	public static void main(String[] args) {
